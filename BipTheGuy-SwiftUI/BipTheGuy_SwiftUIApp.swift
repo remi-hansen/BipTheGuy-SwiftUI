34ac2ce985056f020314802f42c0fc362132beff
Remi Hansen <hansenrd@bc.edu>
2022-09-30 22:42:20 -0400

    Initial Commit

---
+++ BipTheGuy-SwiftUI/BipTheGuy_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuy_SwiftUIApp.swift
+//  BipTheGuy-SwiftUI
+//
+//  Created by Remi Pacifico Hansen on 9/30/22.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuy_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
